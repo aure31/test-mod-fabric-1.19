30e8a4b54ad73ad93c6a6a018c54341bc2877fcc
julian <unconfigured@null.spigotmc.org>
2022-10-30 14:46:26 +0100

    add eight Ball

--- src/main/java/fr/kedor/tmf/init/item/ModItems.java
+++ src/main/java/fr/kedor/tmf/init/item/ModItems.java
@@ -1,6 +1,7 @@
 package fr.kedor.tmf.init.item;
 
 import fr.kedor.tmf.TMF;
+import fr.kedor.tmf.init.item.custom.EightBallItem;
 import net.minecraft.item.Item;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
@@ -11,6 +12,9 @@ public class ModItems {
             new Item(new Item.Settings().group(ModItemsGroup.MY_GROUP).fireproof()));
     public static final Item RAW_TITANIUM = registerItem("raw_titanium",
             new Item(new Item.Settings().group(ModItemsGroup.MY_GROUP).fireproof()));
+
+    public static final Item EIGHT_BALL = registerItem("eight_ball",
+            new EightBallItem(new Item.Settings().group(ModItemsGroup.MY_GROUP).maxCount(1)));
 
     private static Item registerItem(String name, Item item){
         return Registry.register(Registry.ITEM,new Identifier(TMF.MOD_ID,name),item);
